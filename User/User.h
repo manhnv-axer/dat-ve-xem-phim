7048fed39618fc4d08a442642ed351294a2a50c5
Tấn Trúc <tantrucdev@gmail.com>
2026-09-17 20:37:25 +0700

    cpp properties

--- User/User.h
+++ User/User.h
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-// Lop User la lop co so truu tuong vi co phuong thuc ao thuan tuy.
-// Day la abstract class thu nhat cua he thong.
 class User {
 protected:
     string id;
